f51227073d9bc4e9490bb8dc40151aa1871859fc
Arbusto999 <gaster3d@gmail.com>
2025-11-21 12:59:09 +0100

    Add files via upload

--- compras.py
+++ compras.py
@@ -1,69 +1,22 @@
 import pandas as pd
-
 from datetime import datetime
-
 pinf=pd.read_csv("productos_informatica.csv")
 comp=pd.read_csv("compras.csv")
-
-coste=0
-
-yes="Yes" or "yes" or "Y" or "y" or "Sí" or "Si" or "sí" or "si" or "S" or "s"
-
 def hacer_pedido(id_producto,cantidad):
+    from datetime import datetime
+    pinf=pd.read_csv("productos_informatica.csv")
+    comp=pd.read_csv("compras.csv")
+    fecha=datetime.now()
     coste=(pinf.loc[pinf["ID"]==id_producto]["Lote"])*(pinf.loc[pinf["ID"]==id_producto]["Precio de compra"])*cantidad
+    comp.loc[len(comp)]=[fecha.year,fecha.month,fecha.day,id_producto,cantidad,int(coste.iloc[0]),str((pinf.loc[pinf["ID"]==id_producto]["Proveedor"]).iloc[0])]
+    comp.to_csv("compras.csv", index=False)
 def actualizar_precio_compra(id_producto,nuevo_precio):
-    pass
+    pinf=pd.read_csv("productos_informatica.csv")
+    pinf.loc[pinf["ID"]==id_producto,"Precio de compra"]=nuevo_precio
+    pinf.to_csv("productos_informatica.csv", index=False)
 def pedidos_prod(id_producto):
-    pass
+    comp=pd.read_csv("compras.csv")
+    print(comp[comp["ID"]==id_producto])
 def pedido_mes(mes):
-    pass
-
-def main():
-    inp=""
-    while True:
-        print("Que función quiere realizar? Utilize los números 1-4.\n1. Hacer un pedido.\n2. Actualizar el precio de compra de un producto.\n3. Consultar los pedidos de un producto.\n4. Consultar los pedidos en un mes.")
-        inp=input()
-        try:
-            if 1<=int(inp)<=4:
-                inp=(int(inp))
-                if inp==1:
-                    while True:
-                        print("Introduzca el id del producto.\n")
-                        idp=int(input())
-                        try:
-                            if 0<int(inp)<999: #<---[-]Hay que obtener el máximo id disponible!
-                                break
-                            else:
-                                print("Id inválido\n")
-                        except:
-                            print("Id inválido\n")
-                    while True:
-                        print("Introduzca la cantidad a pedir.\n")
-                        cant=int(input())
-                        try:
-                            if 0<int(cant)<999: #<---[-]Hay que obtener el máximo id disponible!
-                                break
-                            else:
-                                print("Cantidad inválida\n")
-                        except:
-                            print("Cantidad inválida\n")
-                    if input(f"pedir {cant} de [id:{idp}]? Y/N: ") == yes:
-                        hacer_pedido(idp,cant)
-                        print(coste)
-                    elif no == no:
-                        pass
-                    #################################################################
-                if inp==2:
-                    pass
-                if inp==3:
-                    pass
-                if inp==4:
-                    pass
-            else:
-                print("No existe esa función\n")
-        except:
-            print("No existe esa función\n")
-            
-
-main()
-    
+    comp=pd.read_csv("compras.csv")
+    print(comp[comp["Mes"]==mes])
