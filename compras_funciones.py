544282921f430f8454290f4432abeda51c4f4c1b
Arbusto999 <gaster3d@gmail.com>
2025-11-23 17:33:19 +0100

    Update compras_funciones.py

--- compras_funciones.py
+++ compras_funciones.py
@@ -1,14 +1,11 @@
 import pandas as pd
-from datetime import datetime
-pinf=pd.read_csv("productos_informatica.csv")
-comp=pd.read_csv("compras.csv")
 def hacer_pedido(id_producto,cantidad):
     from datetime import datetime
     pinf=pd.read_csv("productos_informatica.csv")
     comp=pd.read_csv("compras.csv")
     fecha=datetime.now()
     coste=(pinf.loc[pinf["ID"]==id_producto]["Lote"])*(pinf.loc[pinf["ID"]==id_producto]["Precio de compra"])*cantidad
-    comp.loc[len(comp)]=[fecha.year,fecha.month,fecha.day,id_producto,cantidad,int(coste.iloc[0]),str((pinf.loc[pinf["ID"]==id_producto]["Proveedor"]).iloc[0])]
+    comp.loc[len(comp)]=[fecha.year,fecha.month,fecha.day,id_producto,cantidad,float(coste.iloc[0]),str((pinf.loc[pinf["ID"]==id_producto]["Proveedor"]).iloc[0])]
     comp.to_csv("compras.csv", index=False)
 def actualizar_precio_compra(id_producto,nuevo_precio):
     pinf=pd.read_csv("productos_informatica.csv")
